df86f562c40e52b53544dc480e3f82bcbe6b809d
Luke Norman <lukenorman@gmail.com>
2020-06-17 17:12:25 -0700

    Update database code to play nicely with Heroku

--- db/index.js
+++ db/index.js
@@ -6,19 +6,29 @@ const pg = require('pg')
 //default true
 const ssl = process.env.DB_SSL === undefined ? true : process.env.DB_SSL
 
-const sequelize = new Sequelize({
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    database: process.env.DB_NAME,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    ssl: ssl,
-    native: ssl,
-    dialectModule: pg,
-    dialectOptions: {
-        ssl: ssl ? undefined : 'require',
-    },
-})
-console.log('New postgres database connection initialized')
+let sequelize
+if (process.env.DATABASE_URL) {
+    sequelize = new Sequelize(process.env.DATABASE_URL)
+    console.log(
+        'New postgres database connection initialized from DATABASE_URL on Heroku'
+    )
+} else {
+    sequelize = new Sequelize({
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        database: process.env.DB_NAME,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        ssl: ssl,
+        native: ssl,
+        dialectModule: pg,
+        dialectOptions: {
+            ssl: ssl ? undefined : 'require',
+        },
+    })
+    console.log(
+        'New postgres database connection initialized from DB_NAME/USER/PASS/HOST'
+    )
+}
 
 module.exports = sequelize
