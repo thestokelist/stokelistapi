a13c0d9e7bce07afcfcbef6059db2ea28bf3653f
Luke Norman <lukenorman@gmail.com>
2020-06-17 17:26:50 -0700

    Update sequelize CLI config for Heroku

--- config/config.js
+++ config/config.js
@@ -1,18 +1,35 @@
 require('dotenv').config() // this is important!
+
+let username = process.env.DB_USER
+let password = process.env.DB_PASS
+let database = process.env.DB_NAME
+let host = process.env.DB_HOST
+
+if (process.env.DATABASE_URL) {
+    const relevantUrl = process.env.DATABASE_URL.split('://')[1]
+    //On heroku, so override with heroku settings
+    username = relevantUrl.split(':')[0]
+    let passwordHost = relevantUrl.split(':')[1]
+    let portDatabase = relevantUrl.split(':')[2]
+    password = passwordHost.split('@')[0]
+    host = passwordHost.split('@')[1]
+    database = portDatabase.split('/')[1]
+}
+
 module.exports = {
     development: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
+        username,
+        password,
+        database,
+        host,
         dialect: 'postgres',
     },
     //Production uses same env variables as development
     production: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
+        username,
+        password,
+        database,
+        host,
         dialect: 'postgres',
     },
 }
