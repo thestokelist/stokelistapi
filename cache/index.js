6c092e282a10134c31bc7a1a4262d145293eb936
Luke <lnorman@ad1.systemadmin.com>
2022-12-15 15:19:53 -0800

    try disabling cache to see if that's causing the memory issues

--- cache/index.js
+++ cache/index.js
@@ -1,23 +1,25 @@
-const NodeCache = require('node-cache')
-const postCache = new NodeCache({
+//const NodeCache = require('node-cache')
+/*const postCache = new NodeCache({
     stdTTL: 30000,
     checkPeriod: 600,
-})
+})*/
+
 const cache = {}
 
 cache.put = (id, data) => {
-    postCache.set(id, data)
+    //postCache.set(id, data)
 }
 cache.get = (id) => {
-    return postCache.get(id)
+    return undefined;
+    //return postCache.get(id)
 }
 cache.del = (id) => {
-    postCache.del(id)
+    //postCache.del(id)
 }
 cache.regenLatest = () => {
     // Rather than regenerate, we just delete it from the cache
     // and let the next request repopulate the cache
-    postCache.del('latest')
+    //postCache.del('latest')
 }
 
 module.exports = cache
