e3790e8718c0c6d524a9e80a2ab63275a03f57de
Luke <lnorman@ad1.systemadmin.com>
2022-12-18 17:44:34 -0800

    sharp memory leak?

--- util/s3.js
+++ util/s3.js
@@ -13,6 +13,7 @@ AWS.config.update({
 })
 
 const s3 = new AWS.S3()
+const sharpInstance = sharp()
 
 //This middleware requires express-request-id
 exports.uploadMiddleware = multer({
@@ -31,7 +32,7 @@ exports.uploadMiddleware = multer({
                     cb(null, `${req.id}.${extension}`)
                 },
                 transform: function (req, file, cb) {
-                    cb(null, sharp())
+                    cb(null, sharpInstance)
                 },
             },
             {
@@ -42,7 +43,7 @@ exports.uploadMiddleware = multer({
                 },
                 transform: function (req, file, cb) {
                     //Set height to 120, width maintaining aspect ratio
-                    cb(null, sharp().resize({ height: 120 }))
+                    cb(null, sharpInstance.resize({ height: 120 }))
                 },
             },
         ],
