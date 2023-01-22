19322f1f5ef8db3f70f9f5896f794b31597811fb
Luke <lnorman@ad1.systemadmin.com>
2023-01-21 17:15:55 -0800

    get AWS region from config

--- util/s3.js
+++ util/s3.js
@@ -9,7 +9,7 @@ const { getSignedUrl } = require('@aws-sdk/s3-request-presigner')
 const bucketName = process.env.AWS_S3_BUCKET
 
 const s3Client = new S3Client({
-    region: 'ca-central-1',
+    region: process.env.AWS_REGION,
     credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
